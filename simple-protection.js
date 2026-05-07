5549ec4a92d5f5b1edde1e150340794576260999
mohamedhany <mo776253@gmail.com>
2026-05-07 22:58:56 +0300

    Delete simple-protection.js

--- simple-protection.js
+++
@@ -1,9 +0,0 @@
-// Simple F12 and Right-Click Blocker - DISABLED
-// All inspection blocking has been removed for full functionality
-(function() {
-    'use strict';
-    
-    // All protection code has been removed
-    // Website now has full functionality without any blocking
-    
-})();
